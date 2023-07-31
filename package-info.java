9ff1024057fca69e5c749329a9a1ad2efff80902
Taichiro Yamamoto <83239842+yamaTai7@users.noreply.github.com>
2023-07-31 16:55:27 -0300

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,5 +1,5 @@
 package ProjDevicesPlayer1;
 
 //First Project made by Taichiro Yamamoto, July 31st 2023.
-//During the Java's OOP course teaching by Eduardo Calabria and Gerson Lopes.
+//During the Java's OOP course teaching by Prof. Eduardo Calabria and Prof. Gerson Lopes.
 //At CESAR SCHOOL's NEXT 2023.
